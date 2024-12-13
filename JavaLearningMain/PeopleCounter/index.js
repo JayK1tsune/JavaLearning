a2dd64735a55617ac55cfec5421f6ff9f0ee2d1d
Jay <91475458+JayK1tsune@users.noreply.github.com>
2024-12-13 00:43:10 +0000

    Added backgrounds

--- JavaLearningMain/PeopleCounter/index.js
+++ JavaLearningMain/PeopleCounter/index.js
@@ -4,12 +4,28 @@
 // let secondCount = 10
 // let count = firstCount + secondCount
 
-// console.log(count)
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-let myAge = 26
-let humanToDogYears = 7
+let count = 0
+let countElement = document.getElementById('count')
+let previousEl = document.getElementById('save-el')
 
-let myDogAge = myAge * humanToDogYears
-console.log(myDogAge)
+function increment() {
+    count += 1
+    countElement.textContent = count
+    console.log(count)
+}
+
+function save() {
+    let countStr = count + " - "
+    previousEl.textContent += countStr
+    //reset count to 0
+    count = 0
+    countElement.textContent = count
+}
+
 
 
